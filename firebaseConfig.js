e43e63133f4241c27aa6a4baff57a456e061bff2
Mohammed Qasim <mohammed.qasim1702@gmail.com>
2025-09-01 01:24:31 +0530

    env removal

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,29 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAfPDOcogDb7kls-1SeyFMsB0VRkihkD20",
+  authDomain: "bughuntr-3f895.firebaseapp.com",
+  projectId: "bughuntr-3f895",
+  storageBucket: "bughuntr-3f895.appspot.com",
+  messagingSenderId: "880398940533",
+  appId: "1:880398940533:web:835cd3f7c8e4083d8d3b3c",
+  measurementId: "G-G9K6NC2J3K",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-let analytics = null;
-if (typeof window !== "undefined") {
-  isSupported().then((supported) => {
-    if (supported) {
-      analytics = getAnalytics(app);
-    }
-  });
-}
+const analytics = getAnalytics(app);
 
 const db = getFirestore(app);
 
