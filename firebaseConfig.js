c0090140e387c2edb132172a7e98174dd43994b2
Maaz Khan <maaz.khan25925@gmail.com>
2025-09-01 00:43:47 +0530

    critical: Moved Firebase config to environment variables.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfPDOcogDb7kls-1SeyFMsB0VRkihkD20",
-  authDomain: "bughuntr-3f895.firebaseapp.com",
-  projectId: "bughuntr-3f895",
-  storageBucket: "bughuntr-3f895.appspot.com",
-  messagingSenderId: "880398940533",
-  appId: "1:880398940533:web:835cd3f7c8e4083d8d3b3c",
-  measurementId: "G-G9K6NC2J3K"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
